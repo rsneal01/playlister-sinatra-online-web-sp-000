7a292a7fa939a7f7e828b09b4736a7f7c196db96
rsneal01 <rsneal01@gmail.com>
2021-03-08 14:45:12 -0800

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,16 +6,20 @@ class SongsController < ApplicationController
         erb :'/songs/index'
     end
 
+    get '/songs/new' do
+        @genres = Genre.all
+        erb :'/songs/new'
+    end
+
     get '/songs/:slug' do
         @song = Song.find_by_slug(params[:slug])
         erb :'/songs/show'
     end
 
-    get '/songs/new' do
-        erb :'/songs/new'
-    end
+    
 
     post '/songs' do
+        
         @song = Song.create(name: params[:song_name], artist: params[:artist_id])
     end
 
